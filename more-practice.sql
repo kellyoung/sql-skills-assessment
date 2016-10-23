fc5c5efc45b61c17cee2da753b0040cf2db0c65f
Kelly Young <kelly.y94@gmail.com>
2016-10-23 08:33:22 +0000

    added commands to more-practice.sql

---
+++ more-practice.sql
@@ -0,0 +1,23 @@
+-- Include your solutions to the More Practice problems in this file.
+
+
+
+-- INSERT
+INSERT INTO models (year, brand_name, name)
+VALUES (2015, 'Chevrolet', 'Malibu');
+
+INSERT INTO models (year, brand_name, name)
+VALUES (2015, 'Subaru', 'Outback');
+
+
+-- CREATE TABLE
+CREATE TABLE awards (name VARCHAR(50),
+                     year INTEGER,
+                     winner_id INTEGER PRIMARY KEY);
+
+-- More INSERT
+INSERT INTO awards (name, year, winner_id)
+VALUES ('IIHS Safety Award' 2015, 49);
+
+INSERT INTO awards (name, year, winner_id)
+VALUES ('IIHS Safety Award' 2015, 50);
